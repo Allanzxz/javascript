e59b887ef56c1c28a13a05ee67ba992a690520ba
Allanzxz <allanevangelistabotelho@gmail.com>
2024-03-25 09:25:08 -0400

    finalizado questao 26

---
+++ questao26.js
@@ -0,0 +1,7 @@
+/*
+Questão 26: Como você pode usar o operador ternário em JavaScript?
+*/
+RESPOSTA:
+let numero = 5;
+let resultado = numero > 0 ? 'Positivo' : 'Negativo';
+console.log(resultado); // Isso imprimirá 'Positivo' porque 5 é maior que 0
