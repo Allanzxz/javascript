af4e0d4cf73b22c962eda3abec42914ed79b684e
Allanzxz <allanevangelistabotelho@gmail.com>
2024-03-25 09:22:36 -0400

    finalizado questao 14

---
+++ questao14.js
@@ -0,0 +1,8 @@
+/*
+Questão 14: Como você pode converter uma string em minúsculas em
+JavaScript?
+*/
+RESPOSTA:
+let string = "Olá Mundo";
+let stringMinuscula = string.toLowerCase();
+console.log(stringMinuscula); // Saída: "olá mundo"
