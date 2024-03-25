cbe0786000bfc71472b97305ed346e7e5de4cf05
Allanzxz <allanevangelistabotelho@gmail.com>
2024-03-25 09:25:26 -0400

    finalizado questao 28

---
+++ questao28.js
@@ -0,0 +1,9 @@
+/*
+Questão 28: Como você pode converter uma string em um número inteiro em
+JavaScript?
+*/
+RESPOSTA:
+let stringNumero = "123";
+let numeroInteiro = parseInt(stringNumero);
+
+console.log(numeroInteiro); // Saída: 123
