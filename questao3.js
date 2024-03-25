31f6ae4e50326473789982decb791425a543f124
Allanzxz <allanevangelistabotelho@gmail.com>
2024-03-25 09:26:30 -0400

    finalizado questao 3

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript?
+*/
+
+let nome = "Allan"
+let sobrenome = "Evangelista"
+let resultado;
+
+resultado = nome +" "+ sobrenome;
+console.log(resultado);
