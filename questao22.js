83250f39ec394d6c11cca66589d5343e74092e92
Allanzxz <allanevangelistabotelho@gmail.com>
2024-03-25 09:24:14 -0400

    finalizado questao 22

---
+++ questao22.js
@@ -0,0 +1,8 @@
+/*
+Questão 22: Como você pode verificar se uma variável é nula em JavaScript?
+*/
+RESPOSTA:
+while (condição) {
+    // Código a ser executado enquanto a condição for verdadeira
+}
+
